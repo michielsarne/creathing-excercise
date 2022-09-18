8acb4c009f5ac5d0fdf6e41f3f7a59a7a379843f
michielsarne <arne.michiels@student.howest.be>
2022-09-18 16:58:27 +0200

    content dynamisch & js fix

---
+++ craft/web/scripts/dataHandler.js
@@ -0,0 +1,35 @@
+'use strict';
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const handleData = function (url, callbackFunctionName, callbackErrorFunctionName = null, method = 'GET', body = null) {
+  fetch(url, {
+      method: method,
+      body: body,
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+    )
+    .then(function (response) {
+      if (!response.ok) {
+        delay(10100)
+        // console.warn(`>> Probleem bij de fetch(). Statuscode: ${response.status}`);
+        if (callbackErrorFunctionName) {
+          // console.warn(`>> Callback errorfunctie ${callbackErrorFunctionName.name}(response) wordt opgeroepen`);
+          callbackErrorFunctionName(response);
+        } else {
+          // console.warn('>> Er is geen callback errorfunctie meegegeven als parameter');
+        }
+      } else {
+        // console.info('>> Er is een response teruggekomen van de server');
+        return response.json();
+      }
+    })
+    .then(function (jsonObject) {
+      if (jsonObject) {
+        // console.info('>> JSONobject is aangemaakt');
+        // console.info(`>> Callbackfunctie ${callbackFunctionName.name}(response) wordt opgeroepen`);
+        callbackFunctionName(jsonObject);
+      }
+    });
+
+};
